59633c29a2aaf7cb479322404c6edd89dba1f702
keonly <70371393+keonly@users.noreply.github.com>
2024-12-18 14:45:08 +0900

    fix: separate exmample files for day 17

--- 2024/src/bin/17.rs
+++ 2024/src/bin/17.rs
@@ -249,13 +249,17 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, Some(String::from("5,7,3,0")));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
+        assert_eq!(result, Some(String::from("4,6,3,5,6,3,5,2,1,0")));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, Some(117440));
     }
 }
